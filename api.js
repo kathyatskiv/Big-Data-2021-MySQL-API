339305c54b64a54490a03c9f9892e7f24a67f6cd
Yatskiv Kath <iamkate4@gmail.com>
2021-03-28 00:23:17 +0200

    MVP MySQL API

---
+++ api.js
@@ -0,0 +1,193 @@
+const express = require("express");
+const app = express();
+
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host : 'hostname',
+    user : 'user',
+    password : 'password',
+    database : 'database',
+    dateStrings: true
+});
+ 
+app.get("/", (request, response) => {
+  pool.query('ALTER TABLE products ADD PRIMARY KEY (product_id)',
+  function(err, results){
+    if(err) console.log(err)
+    else response.send(results);
+  })
+  //response.end(`<h1>This is the first homework from the course Big Data Processing 2021</h1>`)
+});
+
+//Return all reviews for specified `product_id`
+//Return all reviews for specified `product_id` with given `star_rating`
+app.route('/reviews/products/:product_id')
+  .get(function(request, response, next) {
+    let id = request.params["product_id"];
+    let rate = request.query.star_rating;
+
+    if(rate == undefined) {
+      pool.query(`
+      SELECT review_headline, review_body 
+      FROM reviews 
+      WHERE product_id=${id}`, 
+      function(err, results) {
+          if(err) {
+              console.log("ERROR\n");
+              console.log(err);
+          }   
+          response.send(results)
+      });
+
+    } else{
+      pool.query(`
+      SELECT review_headline, review_body, star_rating 
+      FROM reviews 
+      WHERE product_id=${id} AND star_rating=${rate}`, 
+      function(err, results) {
+        if(err) {
+            console.log("ERROR\n");
+            console.log(err);
+        }   
+        response.send(results)
+    });
+    }
+  });
+
+//Return all reviews for specified `customer_id`
+app.route('/reviews/customers/:customer_id')
+  .get(function(request, response, next) {
+    let id = request.params["customer_id"];
+    pool.query(`
+    SELECT review_headline, review_body 
+    FROM reviews 
+    WHERE customer_id=${id}`, 
+    function(err, results) {
+        if(err) {
+            console.log("ERROR\n");
+            console.log(err);
+        }   
+        response.send(results)
+    });
+  });
+
+//Return N most reviewed items (by # of reviews) for a given period of time
+app.route('/reviews/popular')
+  .get(function(request,response,next) {
+    let n = request.query.n;
+    let date = request.query.date;
+
+
+    pool.query(`
+    SELECT product_title, reviews_amount 
+    FROM 
+      (SELECT product_id, COUNT(review_id) AS reviews_amount , review_date
+      FROM book_reviews.reviews GROUP BY product_id
+      WHERE review_date = ${date}) AS CountReviews, book_reviews.products AS Products 
+    WHERE Products.product_id = CountReviews.product_id 
+    ORDER BY reviews_amount DESC 
+    LIMIT ${ n == undefined ? 1 : n};`, 
+    function(err, results) {
+      if(err) {
+          console.log("ERROR\n");
+          console.log(err);
+      }   
+      response.send(results)
+  });
+  })
+
+//Return N most productive customers (by # of reviews written for verified purchases) for a given period
+app.route('/customers/productive')
+  .get(function(request,response,next) {
+    let n = request.query.n;
+
+    pool.query(`
+    SELECT customer_id, COUNT(review_id) AS reviews_amount
+    FROM reviews
+    WHERE verified_purchase='Y'
+    GROUP BY customer_id
+    ORDER BY reviews_amount DESC
+    LIMIT ${ n == undefined ? 1 : n};`, 
+    function(err, results) {
+      if(err) {
+          console.log("ERROR\n");
+          console.log(err);
+      }   
+      response.send(results)
+  });
+  })
+
+//Return N best products by fraction of 5-star reviews (among products that have >100 of verified reviews)
+app.route('/rating')
+  .get(function(request,response,next) {
+    let n = request.query.n;
+    let limit = 100 //among products that have >100 of verified reviews
+
+    pool.query(`
+    SELECT products.product_title, X.product_rating
+    FROM
+      (SELECT product_id, reviews_amount, product_rating
+      FROM 
+        (SELECT product_id, AVG(star_rating) AS product_rating, COUNT(star_rating) AS reviews_amount
+        FROM
+          (SELECT product_id, star_rating, verified_purchase
+                FROM reviews
+                WHERE verified_purchase = 'Y') AS VerifiedReviews
+        GROUP BY product_id
+        ORDER BY product_rating DESC) AS CountRating
+      WHERE reviews_amount > ${limit}) AS X, products
+    WHERE X.product_id = products.product_id
+    LIMIT ${ n == undefined ? 1 : n};`, 
+    function(err, results) {
+      if(err) {
+          console.log("ERROR\n");
+          console.log(err);
+      }   
+      response.send(results)
+  });
+  })
+
+//Return N most productive “haters” (by # of 1- or 2-star reviews) for a given period
+app.route('/haters')
+  .get(function(request,response,next) {
+    let n = request.query.n;
+
+    pool.query(`
+    SELECT customer_id, COUNT(star_rating) AS negative_reviews_amount
+    FROM reviews
+    WHERE star_rating = '1' OR star_rating = '2'
+    GROUP BY customer_id
+    ORDER BY negative_reviews_amount DESC
+    LIMIT ${ n == undefined ? 1 : n};`, 
+    function(err, results) {
+      if(err) {
+          console.log("ERROR\n");
+          console.log(err);
+      }   
+      response.send(results)
+  });
+  })
+
+//Return N most productive “backers” (by # of 4- or 5-star reviews) for a given period
+app.route('/backers')
+  .get(function(request,response,next) {
+    let n = request.query.n;
+
+    pool.query(`SELECT customer_id, COUNT(star_rating) AS positive_reviews_amount
+    FROM reviews
+    WHERE star_rating = '4' OR star_rating = '5'
+    GROUP BY customer_id
+    ORDER BY positive_reviews_amount DESC
+    LIMIT ${ n == undefined ? 1 : n};`, function(err, results) {
+      if(err) {
+          console.log("ERROR\n");
+          console.log(err);
+      }   
+      response.send(results)
+  });
+  })
+
+
+app.set('port', process.env.PORT || 3306);
+app.listen(3306);
---
+++ data-convertor.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+import unicodedata as ud
+
+engine = create_engine("mysql://USER:PASSWORD@HOST/DATABASE?charset=utf8mb4")
+conn = engine.connect()
+
+
+c_size = 100_000
+
+for data_chunk in pd.read_csv('../data/data.tsv', sep='\t', chunksize=c_size, error_bad_lines=False, encoding='utf8'):
+    customres = data_chunk[['customer_id']].drop_duplicates().copy()
+    products = data_chunk[['marketplace','product_id','product_parent','product_title','product_category']].drop_duplicates(subset=['product_id']).copy()
+    reviews = data_chunk[['customer_id', 'review_id', 'product_id', 'star_rating', 'helpful_votes', 'total_votes', 'vine', 'verified_purchase', 'review_headline', 'review_body', 'review_date']].drop_duplicates(subset=['review_id']).copy()
+
+    print(reviews.head())
+
+    try:
+        products.to_sql('products', conn, if_exists = 'append', index = False)
+    except sa.exc.IntegrityError:
+        pass
+    except sa.exc.OperationalError:
+        pass
+
+    try:
+        customres.to_sql('customers', conn, if_exists = 'append', index = False)
+    except sa.exc.IntegrityError:
+        pass
+    except sa.exc.OperationalError:
+        pass
+
+    try:
+        reviews.to_sql('reviews', conn, if_exists = 'append', index = False)
+    except sa.exc.IntegrityError:
+        pass
+    except sa.exc.OperationalError:
+        pass
+
+    print('Chunk loaded')
+
+
+
+
+conn.close()
